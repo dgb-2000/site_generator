b26d53c96ef66c685dd50ba28203be411138744d
dgb-2000 <daniel@basnar.tech>
2025-10-11 22:51:37 +0200

    added textnode to htmlnode converter

--- src/leafnode.py
+++ src/leafnode.py
@@ -6,7 +6,7 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             raise ValueError
 
         if self.tag is None:
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from converters import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -28,6 +29,34 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+    def test_text_to_html(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold_to_html(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_image_to_html(self):
+        node = TextNode("This is an image node", TextType.IMAGE, "https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(
+            html_node.to_html(),
+            '<img src="https://boot.dev" alt="This is an image node"></img>'
+        )
+
+    def test_link_to_html(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(
+            html_node.to_html(),
+            '<a href="https://boot.dev">This is a link node</a>'
         )
 
 

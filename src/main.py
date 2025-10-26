503540c53b4483c7ea381a39cb639a0ba4c85355
dgb-2000 <daniel@basnar.tech>
2025-10-26 20:01:16 +0100

    added function to recursively copy static/ into public/

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_static(dir):
+    if os.path.exists(dir):
+        for item in os.listdir(dir):
+            path = os.path.join(dir, item)
+            new_dir = ""
+            if "/" in dir:
+                new_dir = os.path.join("public", dir.split("/", 1)[1])
+            else:
+                new_dir = "public"
+            if os.path.isfile(path):
+                if not os.path.exists(new_dir):
+                    os.mkdir(new_dir)
+                shutil.copy(path, os.path.join(new_dir, item))
+            else:
+                if not os.path.exists(new_dir):
+                    os.mkdir(new_dir)
+                copy_static(path)
 
 
 def main():
-    node = TextNode("lorem ipsum", TextType.LINK, "https://boot.dev")
-    print(node)
+    shutil.rmtree("public")
+    os.mkdir("public")
+    copy_static("static")
 
 
 if __name__ == "__main__":
